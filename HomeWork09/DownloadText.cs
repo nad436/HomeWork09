cfd7fddccfc409104cc80a40cfe09a09cccf5450
nad436 <www.tanya0197@gmail.com>
2024-09-12 20:05:50 +0200

    Add project files.

---
+++ HomeWork09/DownloadText.cs
@@ -0,0 +1,16 @@
+﻿partial class Program
+{
+    private class DownloadText
+    {
+        public  async void DownloadDataAsync(string url, string filePath)
+        {
+            HttpClient text = new HttpClient();
+
+            string response = await text.GetStringAsync(url);
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+            await File.WriteAllTextAsync(filePath, response);
+            Console.WriteLine($"Content saved to {filePath}");
+        }
+    }
+}
---
+++ HomeWork09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+partial class Program
+{
+    public static async Task  Main(string[] args)
+    {
+        string filePath = @"D:\Text\downloadedPage.txt";
+        string url = "https://www.antimoon.com/";
+
+        int number1 = 7, number2 = 8;
+
+        var text = new DownloadText();
+
+        var task1 = new Task(() => Console.WriteLine("Hello World"));
+        var task2 = new Task(() => Thread.Sleep(1000));
+        var task3 = new Task(() => Console.WriteLine(number1 + number2));
+        var task4 = new Task(() => text.DownloadDataAsync(url, filePath));
+
+        task1.Start();
+        task2.Start();
+        task3.Start();
+        task4.Start();
+
+        Task.WaitAll(task1, task2, task3, task4);
+       
+
+        
+        
+    }
+}
